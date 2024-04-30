6904f02c2aa6b0e15b2a3f2c29a86527fbbf19e2
rauzerde <rauzerde@naver.com>
2024-04-30 13:25:49 +0900

    first commit
    1st

---
+++ test.cpp
@@ -0,0 +1,61 @@
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("First");
+LPCSTR lpsClass = "First";
+LPCWSTR lpswClass = L"First\0";
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
+	, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = 0;
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+	
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd,UINT iMessage,WPARAM wParam,LPARAM lParam)
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+
+	switch (iMessage) {
+        case WM_PAINT:
+        {
+            hdc = BeginPaint(hWnd, &ps);
+            //TextOutW(hdc, 100, 100, lpswClass, 13);
+            EndPaint(hWnd, &ps);
+            break;
+        }
+	    case WM_DESTROY:
+        {
+		    PostQuitMessage(0);
+		    break;
+        }
+	}
+	return DefWindowProc(hWnd, iMessage, wParam, lParam);
+}
